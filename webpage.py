0aa5e26a722308d1c46f0a08eba5abfbb2101641
SiLordOfLight <jatnerd@gmail.com>
2018-10-23 15:20:10 -0700

    sadfa

--- webpage.py
+++ webpage.py
@@ -9,13 +9,35 @@ def render_main():
 @app.route("/response")
 def render_response():
     USD = float(request.args['USD'])
+    reply = USD * 19.35
+    return render_template('response.html',response='$' + str(reply))
+    
+@app.route("/page1")
+def render_er():
+    return render_template('page1.html')
+    
+@app.route("/response1")
+def render_response():
+    feet = float(request.args['feet'])
+    reply = feet * 0.3048
+    return render_template('response.html',str(reply))
+   
+@app.route("/page2")
+def render_q():
+    return render_template('page2.html')
+    
+@app.route("/response2")
+def render_response():
+    feet = float(request.args['pounds'])
+    reply = pounds * 0.453592 
+    return render_template('response.html',str(reply))
+
+    
     #The request object stores information about the request sent to the server.
     #args is a MultiDict (like a dictionary but can have multiple values for the same key)
     #The information in args is visable in the url for the page being requested. ex. .../response?color=orange
 
-    reply = USD * 19.26
-        
-    return render_template('response.html',response='$' + str(reply))
+ 
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
